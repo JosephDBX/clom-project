1c3080d706b71d78336dc2d3ede8b61d1dbe0da0
JosephDBX <st.joseph.dbx@gmail.com>
2021-02-20 13:47:29 -0600

    Adding area slide

---
+++ clom-web-app/src/components/area/AreaSlideItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card :loading="loading" class="ma-1" width="200">
+    <template slot="progress">
+      <v-progress-linear
+        color="accent"
+        height="5"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+      height="150"
+      :lazy-src="member.photoURL"
+      :src="member.photoURL"
+      transition="fade-transition"
+    ></v-img>
+
+    <v-card-title class="body-1">
+      {{ member.name }} {{ member.surname }}
+    </v-card-title>
+
+    <v-card-text>
+      <div class="grey--text body-2 text-justify">
+        {{ member.bio }}
+      </div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-text class="text-center">
+      <div class="body-1">Teléfono: {{ member.contact.phone }}</div>
+
+      <v-btn color="secondary" plain @click="location">Localización</v-btn>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import Member from "../../models/Member";
+
+export default {
+  props: {
+    member: Member,
+  },
+  data: () => ({
+    loading: true,
+    selection: 1,
+  }),
+
+  methods: {
+    location() {
+      this.loading = false;
+    },
+  },
+};
+</script>
--- clom-web-app/src/views/public/Home.vue
+++ clom-web-app/src/views/public/Home.vue
@@ -16,7 +16,7 @@
       </v-col>
     </v-row>
 
-    <v-row class="my-5 my-md-15 mx-md-2 mx-lg-4">
+    <v-row class="my-5 my-md-16 mx-md-2 mx-lg-4">
       <v-col xs="12" md="7" xl="9">
         <member-slide />
       </v-col>
@@ -41,15 +41,52 @@
         </p>
       </v-col>
     </v-row>
+
+    <v-row class="mx-2 mt-5 d-block d-md-none">
+      <v-col cols="12">
+        <p class="text-h6 text-sm-h5 text-center text-sm-left grey--text">
+          Áreas en las que nos especializamos
+        </p>
+
+        <v-divider></v-divider>
+      </v-col>
+    </v-row>
+
+    <v-row class="my-5 my-md-16 mx-md-2 mx-lg-4">
+      <v-col xs="12" md="5" xl="3" class="d-none d-md-block">
+        <p class="text-h6 text-sm-h5 text-center text-sm-left grey--text">
+          Áreas en las que nos especializamos
+        </p>
+
+        <p class="grey--text text-justify">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
+          eum iste optio repudiandae architecto, quod reiciendis, rerum rem
+          dolorem nihil ex! Minus totam consequatur autem mollitia ea atque eos
+          eaque nam! Quos dolor voluptate perferendis esse natus quia inventore
+          obcaecati, tempora ex explicabo adipisci recusandae eligendi molestiae
+          vel, cum cumque sapiente magni consequuntur alias dolorem fugit. Aut,
+          sunt facere deleniti, eos provident, voluptatem maxime cumque
+          accusantium sint eum voluptatum. Rem aliquid laboriosam, odio commodi
+          veniam dolorum magnam neque deleniti? Iusto et, unde minima libero,
+          magnam numquam quam inventore voluptatem quae, corrupti nam. Quis eius
+          excepturi eum fugit delectus architecto illum.
+        </p>
+      </v-col>
+
+      <v-col xs="12" md="7" xl="9">
+        <area-slide />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import AreaSlide from "../../components/area/AreaSlide.vue";
 import Carousel from "../../components/layout/Carousel.vue";
 import MemberSlide from "../../components/member/MemberSlide.vue";
 
 export default {
-  components: { Carousel, MemberSlide },
+  components: { Carousel, MemberSlide, AreaSlide },
   data: () => ({}),
 };
 </script>
