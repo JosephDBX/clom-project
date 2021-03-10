20cef7892e170199405691ba3de61669ed9e7ce1
JosephDBX <st.joseph.dbx@gmail.com>
2021-03-10 16:17:18 -0600

    react-router-dom added

--- clom-web-app/src/App.tsx
+++ clom-web-app/src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.scss";
+import Home from "./views/public/Home";
 
 const App: React.FunctionComponent = () => {
-  return <>
-  </>
+  return <BrowserRouter>
+    <header>HEADER</header>
+
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+
+    <footer>FOOTER</footer>
+  </BrowserRouter>
 }
 
 export default App;
---
+++ clom-web-app/src/views/public/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home: React.FunctionComponent = () => {
+    return <main className="container mx-auto">
+        CONTENT
+    </main>
+}
+
+export default Home;
