31cbc2e21b59132f84c685516d939b694e7fffcf
JosephDBX <st.joseph.dbx@gmail.com>
2021-03-11 15:08:43 -0600

    React web app moved

--- clom-web-app/src/components/layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer: React.FunctionComponent = () => {
-    return <footer className="text-white bg-black p-4">FOOTER</footer>
-}
-
-export default Footer;
--- clom-web-app/src/components/layout/Sidenav.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Sidenav: React.FunctionComponent = () => {
-    return <section className="hidden">SIDENAV</section>
-}
-
-export default Sidenav;
